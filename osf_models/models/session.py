e4bac3bcf3bcac6a4e1845f15f2a8ed8664d725f
Chris Wisecarver <cwisecarver@cos.io>
2016-09-01 10:21:35 -0400

    Force size on session migration

--- osf_models/models/session.py
+++ osf_models/models/session.py
@@ -8,6 +8,7 @@ class Session(ObjectIDMixin, BaseModel):
     # TODO DELETE ME POST MIGRATION
     modm_model_path = 'framework.sessions.model.Session'
     modm_query = None
+    migration_page_size = 30000
     # /TODO DELETE ME POST MIGRATION
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
